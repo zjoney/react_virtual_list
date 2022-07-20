9f654b91faf6b1bc848d2515b0531c1917664667
zjoney <zdjoey@126.com>
2022-07-20 18:05:32 +0800

    💥 feat(模块): 重写react-window

--- src/fixed-size-list.js
+++ src/fixed-size-list.js
@@ -1,31 +1,23 @@
-import React from 'react';
-import { FixedSizeList } from './react-window';
+import { FixedSizeList } from "./react-window";
 import './fixed-size-list.css'
-const Row = ({ index, style, isScrolling }) => {
-  return (
-    <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
-      {isScrolling ? 'scrolling' : ` Row ${index}`}
-    </div>
-  )
-}
 
+const Row = ({ index, style }) => {
+  return <div className={index % 2 ? 'ListItmeOdd' : 'ListItemEven'} style={style}>
+    Row {index}
+  </div>
+}
 function App() {
-  const listRef = React.useRef();
+
   return (
-    <>
-      <button onClick={() => listRef.current.scrollToItem(50)}>50</button>
-      <FixedSizeList
-        className='List'
-        height={200}
-        width={200}
-        itemSize={50}
-        itemCount={1000}
-        useIsScrolling
-        ref={listRef}
-      >
-        {Row}
-      </FixedSizeList>
-    </>
+    <FixedSizeList
+      className="List"
+      height={200}
+      width={200}
+      itemSize={50}
+      itemCount={1000}
+    >
+      {Row}
+    </FixedSizeList>
   )
 }
 export default App;
--- src/react-window/index.js
+++ src/react-window/index.js
@@ -1,2 +1,1 @@
-export { default as FixedSizeList } from './FixedSizeList';
-export { default as VariableSizeList } from './VariableSizeList';
+export {default as FixedSizeList } from './FixedSizeList'
