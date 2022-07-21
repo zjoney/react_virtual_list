ebbc4a1185835630173e84899981c1e99a89da06
zjoney <zdjoey@126.com>
2022-07-21 10:20:44 +0800

    💥 feat(重写react-wndow): 1、根据元素卷起的索引展示条目2、监听滚动事件

--- src/react-window/FixedSizeList.js
+++ src/react-window/FixedSizeList.js
@@ -1,7 +1,14 @@
 import CreateListComponenC from "./createListComponent";
 
 const FixedSizeList = CreateListComponenC({
-
+  getItemSize: ({ itemSize }) => itemSize,//每个条目的高度
+  getEstimatedTotalSize: ({ itemSize, itemCount }) => itemSize * itemCount, //获取预计的总高度
+  getItemOffset: ({ itemSize }, index) => itemSize * index, //获取每个条目的偏移量
+  getStartIndexForOffset: ({ itemSize }, offset) => Math.floor(offset / itemSize),//获取起始索引
+  getStopIndexForStartIndex: ({ height, itemSize }, startIndex) => {//获取结束索引
+    const numVisibleItems = Math.ceil(height / itemSize);
+    return startIndex + numVisibleItems - 1;
+  },
 })
 
 export default FixedSizeList
--- src/react-window/createListComponent.js
+++ src/react-window/createListComponent.js
@@ -1,21 +1,56 @@
 import React from "react";
 
 function createListComponent({
-
+  getItemSize,//每个条目的高度
+  getEstimatedTotalSize, //获取预计的总高度
+  getItemOffset, //获取每个条目的偏移量
+  getStartIndexForOffset, // 根据向上卷起的高度计算开始索引
+  getStopIndexForStartIndex,//获取结束索引 
 }) {
   return class extends React.Component {
+    state = { scrollOffset: 0 }
     render() {
-      const { width, height } = this.props;
+      const { width, height, itemCount, children: ComponentType } = this.props;
       const containerStyle = { position: 'relative', width, height, overflow: 'auto', willChange: 'transform' }
+      const contentStyle = { width: '100%', height: getEstimatedTotalSize(this.props) };
+      const items = [];
+      if (itemCount > 0) {
+        const [startIndex, stopIndex] = this.getRangeToRender()
+        for (let index = startIndex; index <= stopIndex; index++) {
+          items.push(
+            <ComponentType key={index} index={index} style={this._getItemStyle(index)} />
+          );
+        }
+      }
       return (
-        <div style={containerStyle}>
-          <div>
-            
+        <div style={containerStyle} onScroll={this.onScroll}>
+          <div style={contentStyle}>
+            {items}
           </div>
         </div>
       )
     }
+    onScroll=(event)=>{
+      const { scrollTop } = event.currentTarget;
+      this.setState({ scrollOffset: scrollTop });
+    }
+    getRangeToRender = () => {
+      const { scrollOffset } = this.state;
+      const startIndex = getStartIndexForOffset(this.props, scrollOffset);
+      const stopIndex = getStopIndexForStartIndex(this.props, startIndex);
+      return [startIndex, stopIndex];
+    
   }
+  _getItemStyle = (index) => {
+    const style = {
+      position: 'absolute',
+      width: '100%',
+      height: getItemSize(this.props),
+      top: getItemOffset(this.props, index)
+    };
+    return style;
+  }
+}
 }
 
 export default createListComponent;
